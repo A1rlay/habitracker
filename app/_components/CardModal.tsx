bd740521fb063ed2aaba6f1966c3b35957d541ea
a1rlay <a1rlay@outlook.com>
2026-02-08 00:44:43 -0700

    Added localstorage to store data persistently

--- app/_components/CardModal.tsx
+++ app/_components/CardModal.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 type Card = {
   id: number;
@@ -9,7 +9,7 @@ type Card = {
 
 function ItemRow({ id, name, description }: Card) {
   return (
-    <div className="bg-[#656d4a] text-[#f9f9f9] py-4 px-8 rounded-2xl mb-8">
+    <div className="bg-[#656d4a] text-[#f9f9f9] py-4 px-8 rounded-2xl mb-8 min-w-64 max-w-3/4 m-auto">
       <h3 className="font-bold text-xl">
         {id}. {name}
       </h3>
@@ -19,7 +19,7 @@ function ItemRow({ id, name, description }: Card) {
 };
 
 export default function CreateCard() {
-  const [items, setItems] = useState<Card[]>([]);
+  const [cards, setCards] = useState<Card[]>([]);
   const [isOpen, setIsOpen] = useState(false);
 
   const [name, setName] = useState("");
@@ -39,7 +39,7 @@ export default function CreateCard() {
 
     if (!trimmedName || !trimmedDescription) return;
 
-    setItems(prev => [
+    setCards(prev => [
       ...prev,
       {
         id: prev.length + 1,
@@ -51,50 +51,76 @@ export default function CreateCard() {
     closeModal();
   };
 
+  useEffect(() => {
+    const savedCards = localStorage.getItem("cards");
+
+    if (!savedCards) return;
+
+    try {
+      const parsed: Card[] = JSON.parse(savedCards);
+      setCards(parsed);
+    } catch {
+      localStorage.removeItem("cards");
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("cards", JSON.stringify(cards));
+  }, [cards]);
+
   return (
-    <div className="mt-24">
-      {items.map(c => (
+    <div className="mt-20">
+      {cards.map(c => (
         <ItemRow key={c.id} id={c.id} name={c.name} description={c.description} />
       ))}
 
-      <button onClick={openModal} className="block mt-16 mb-24 bg-[#656d4a] px-6 py-2 rounded-2xl text-[#f9f9f9] m-auto">Add card</button>
+      <button
+        onClick={openModal}
+        className="block mt-12 mb-24 bg-[#656d4a] px-12 py-3 rounded-4xl text-[#f9f9f9] m-auto font-semibold text-2xl">
+        Add card
+      </button>
 
       {isOpen && (
         <div
           role="dialog"
           aria-modal="true"
           onClick={closeModal}
-          className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+          className="fixed top-[40%] left-1/2 transform -translate-x-1/2 -translate-y-1/2"
         >
           <div
             onClick={e => e.stopPropagation()}
             className="w-full max-w-[480] bg-[#7f5539] text-[#f9f9f9] rounded-xl p-16"
           >
-            <h2 className="m-0 text-[1.25em] font-semibold">Create card</h2>
+            <h2 className="m-0 text-3xl font-semibold">
+              Create card
+            </h2>
 
-            <label className="block mt-4">
+            <label className="block mt-4 text-xl">
               Title
               <input
                 value={name}
                 onChange={e => setName(e.target.value)}
-                className="w-full p-2 mt-2 bg-[#a68a64] text-sm"
+                className="w-full p-3 mt-2 bg-[#a68a64] text-base rounded text-black font-semibold"
                 placeholder="FundMiner"
               />
             </label>
 
-            <label className="block mt-4">
+            <label className="block mt-4 text-xl">
               Description
               <textarea
                 value={description}
                 onChange={e => setDescription(e.target.value)}
-                className="w-full p-2 mt-2 bg-[#a68a64] text-sm"
+                className="w-full p-3 mt-2 bg-[#a68a64] text-base rounded text-black font-semibold min-h-12 max-h-80"
                 placeholder="Complete FUN-1392"
               />
             </label>
 
-            <div className="flex gap-8 justify-end mt-8">
-              <button onClick={closeModal} className="hover:cursor-pointer">Cancel</button>
-              <button onClick={saveCard} disabled={!name.trim() || !description.trim()} className="hover:cursor-pointer">
+            <div className="flex gap-6 justify-end mt-12">
+              <button onClick={closeModal} className="hover:cursor-pointer bg-[#c1121f] px-4 py-2 rounded-2xl">
+                Cancel
+              </button>
+              <button onClick={saveCard} disabled={!name.trim() || !description.trim()}
+                className="hover:cursor-pointer bg-[#606c38] px-4 py-2 rounded-2xl">
                 Save
               </button>
             </div>
